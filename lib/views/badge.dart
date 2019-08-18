8ab5c251be67b1c4b8d8cfda8394f687e954242b
yx544806988 <yx891167>
2019-08-18 14:50:19 +0800

    update badge demo

--- lib/views/badge.dart
+++ lib/views/badge.dart
@@ -9,9 +9,7 @@ class Badge extends StatelessWidget {
       body: ListView(
         children: <Widget>[
           TitleView('Badge', '徽章'),
-          ListTile(
-            title: Text('正常使用'),
-          ),
+          ListTile(title: Text('正常使用')),
           Padding(
             padding: EdgeInsets.only(left: 32),
             child: WeBadge(
@@ -19,35 +17,33 @@ class Badge extends StatelessWidget {
             ),
           ),
           SizedBox(height: 10),
-          ListTile(
-            title: Text('空心'),
-          ),
+          ListTile(title: Text('size状态big')),
           Padding(
             padding: EdgeInsets.only(left: 32),
             child: WeBadge(
-              child: '11122',
+              child: '这是大样式',
+              size: SizeType.big,
+            ),
+          ),
+          SizedBox(height: 10),
+          ListTile(title: Text('size状态small')),
+          Padding(
+            padding: EdgeInsets.only(left: 32),
+            child: WeBadge(
+              child: '这是小样式',
+              size: SizeType.samll,
+            ),
+          ),
+          SizedBox(height: 10),
+          ListTile(title: Text('空心')),
+          Padding(
+            padding: EdgeInsets.only(left: 32),
+            child: WeBadge(
+              child: '这是空心',
               hollow: true,
             ),
           ),
-          ListTile(
-            title: Text('自定义字体'),
-          ),
-          Padding(
-            padding: EdgeInsets.only(left: 32),
-            child: WeBadge(
-              child: '8',
-              textStyle: TextStyle(fontSize: 8),
-              padding: EdgeInsets.only(
-                right: 3.0,
-                top: 1,
-                left: 3.0,
-                bottom: 1,
-              ),
-            ),
-          ),
-          ListTile(
-            title: Text('自定义内间距'),
-          ),
+          ListTile(title: Text('自定义内间距')),
           Padding(
             padding: EdgeInsets.only(left: 32),
             child: WeBadge(
--- ui/lib/badge/index.dart
+++ ui/lib/badge/index.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+enum SizeType { big, samll, custom }
+
 ///数字标识
 class WeBadge extends StatelessWidget {
   final child;
@@ -8,6 +10,7 @@ class WeBadge extends StatelessWidget {
   final Border border;
   final EdgeInsetsGeometry padding;
   final bool hollow;
+  final SizeType size;
 
   WeBadge({
     this.child,
@@ -16,6 +19,7 @@ class WeBadge extends StatelessWidget {
     this.border,
     this.padding,
     this.hollow = false,
+    this.size = SizeType.big,
   });
 
   @override
@@ -25,13 +29,22 @@ class WeBadge extends StatelessWidget {
     Border boxBorder;
     BorderRadius radius = BorderRadius.all(Radius.circular(50));
 
-    EdgeInsetsGeometry boxPadding = padding ??
-        EdgeInsets.only(
-          top: 2.0,
-          right: 6.0,
-          bottom: 2.0,
-          left: 6.0,
-        );
+    EdgeInsetsGeometry boxPadding;
+    double fontSize;
+
+    if (size == SizeType.custom) {
+      boxPadding = padding ??
+          EdgeInsets.only(top: 2.0, right: 6.0, bottom: 2.0, left: 6.0);
+      fontSize = 13.0;
+    } else if (size == SizeType.samll) {
+      boxPadding =
+          EdgeInsets.only(top: 1.0, right: 3.0, bottom: 1.0, left: 3.0);
+      fontSize = 8;
+    } else {
+      boxPadding =
+          EdgeInsets.only(top: 2.0, right: 6.0, bottom: 2.0, left: 6.0);
+      fontSize = 13.0;
+    }
 
     if (hollow) {
       textColor = color;
@@ -56,7 +69,7 @@ class WeBadge extends StatelessWidget {
             padding: boxPadding,
             child: DefaultTextStyle(
               style: TextStyle(
-                fontSize: 13,
+                fontSize: fontSize,
                 color: textColor,
               ),
               child: Text(child, style: textStyle),
